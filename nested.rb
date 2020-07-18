ff9afaedae936713f172cb67aebcea901202b05c
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-18 03:19:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,9 +104,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-    alan_kay =p rogrammer_hash[:alan_kay]
+    alan_kay = programmer_hash[:alan_kay]
    alans_new_info = alan_kay[:known_for]
-    alans_new_info= "GUI"
+    alans_new_info << "GUI"
 end
 
 def adding_to_dennis
